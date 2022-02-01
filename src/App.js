263e660e2d3fb92d81002f77fb5b8b3cb0a14e64
Daniel Constantin <hello@danielconstantin.net>
2022-02-01 20:06:49 +0200

    Add unsplash api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const baseUrl = "https://source.unsplash.com/1920x1080/?city"
+
+function App () {
+  const [imageSrc, setImageSrc] = useState()
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then(response => response.blob())
+      .then(URL.createObjectURL)
+      .then(setImageSrc);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {imageSrc && <img src={imageSrc} alt="logo" />}
     </div>
   );
 }
