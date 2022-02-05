a3dbf3d4f5c938fd000cccfb6f12a04a22e6cabd
Daniel Constantin <hello@danielconstantin.net>
2022-02-05 21:55:46 +0200

    Fix unsplash caching

--- src/api.js
+++ src/api.js
@@ -1,15 +1,26 @@
 import axios from "axios";
+import { wait } from "./utils";
 
-const baseUrl = "https://source.unsplash.com/1920x1080/?moon";
+const baseUrl = "https://source.unsplash.com/1920x1080/?new-york";
 
 export async function fetchImageUrl(config = {}) {
-  const response = await axios.get(baseUrl, {
+  return axios.get(baseUrl, {
     responseType: "blob",
     ...config,
   });
-  console.log(
-    "📜 LOG > fetchImageUrl > response",
-    response.request.responseURL
-  );
-  return URL.createObjectURL(response.data);
+}
+
+let lastImageUrl = "";
+
+export async function fetchNewImageUrl(config = {}) {
+  const response = await fetchImageUrl(config);
+  const uniqueImageUrl = response.request.responseURL.split("?")[0];
+
+  if (lastImageUrl === uniqueImageUrl) {
+    await wait(500); // prevent spam
+    return fetchNewImageUrl(config);
+  } else {
+    lastImageUrl = uniqueImageUrl;
+    return response;
+  }
 }
--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -7,8 +7,7 @@ import {
   LinearProgress,
 } from "@mui/material";
 import styled from "@emotion/styled";
-import { fetchImageUrl } from "../api";
-import { wait } from "../utils";
+import { fetchNewImageUrl } from "../api";
 
 const MainContainer = styled.div`
   height: 100%;
@@ -42,53 +41,31 @@ const ProgressBar = styled(LinearProgress, {
 
   background: none;
 
-  & > .MuiLinearProgress-bar {
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-  }
-
-  &[aria-valuenow="0"] {
-    transition: none;
-
-    & > .MuiLinearProgress-bar {
-      transition: none;
-    }
-  }
-
-  transition: opacity 1s linear;
+  transition: opacity 0.225s linear;
   opacity: ${(props) => (props.visible ? 1 : 0)};
 `;
 
 export function FrontPage() {
   const [imageSrc, setImageSrc] = useState();
-  const [progress, setProgress] = useState(100);
+  const [loading, setLoading] = useState(false);
 
   const refreshImage = useCallback(async () => {
-    setProgress(0);
-    await wait(500);
-    const imgSrc = await fetchImageUrl({
-      onDownloadProgress: (progressEvent) => {
-        const percentage = Math.round(
-          (progressEvent.loaded * 100) / progressEvent.total
-        );
-        setProgress(percentage);
-        console.log("📜 LOG > refreshImage > percentage", percentage);
-      },
-    });
-    console.log("📜 LOG > refreshImage > imgSrc", imgSrc);
-    setImageSrc(imgSrc);
+    setLoading(true);
+
+    const response = await fetchNewImageUrl();
+    setImageSrc(URL.createObjectURL(response.data));
+
+    setLoading(false);
   }, []);
 
   useEffect(() => {
     refreshImage();
   }, [refreshImage]);
 
-  const loading = progress !== 100;
-
   return (
     <MainContainer>
       <Background style={{ backgroundImage: `url(${imageSrc})` }} />
-      <ProgressBar variant="determinate" value={progress} visible={loading} />
+      <ProgressBar variant="indeterminate" visible={loading} />
       <ButtonContainer>
         <IconButton
           aria-label="refresh image"
