f49ff84dd8a52fb7899d46ce471b7df79722ab08
Daniel Constantin <hello@danielconstantin.net>
2023-05-07 23:48:55 +0300

    Return unsplash photo metadata

--- packages/web/src/unsplash-api.ts
+++ packages/web/src/unsplash-api.ts
@@ -1,24 +1,48 @@
 import { blobToBase64, wait } from "./utils";
+import { Random } from "unsplash-js/dist/methods/photos/types"
 
 const ACCESS_KEY = "kgy2dYcQjx4oeo5STQD8zKIYiozmj8ZqHFOi5Pi2pDs"
 const RANDOM_IMAGE_URL = `https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}`; // ?space
 
-export async function fetchRandomImage(): Promise<string> {
-  const response = await fetch(RANDOM_IMAGE_URL);
-  const body = await response.json();
-  console.log("📜 LOG > fetchRandomImage > body.urls.regular:", body.urls.regular);
-  const imgUrl = body.urls.full as string;
-  // await wait(1500)
-  // const imgUrl = 'https://images.unsplash.com/photo-1637270868031-b28f517e152e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+type RandomApiResponse = Random & {
+  views: number
+  tags_preview: Array<{
+    type: string,
+    title: string
+  }>
+}
 
+export async function fetchRandomImage() {
+  const response = await fetch(RANDOM_IMAGE_URL);
+  const body: RandomApiResponse = await response.json();
+  //
+  const downloadUrl = body.urls.full;
+  // fix type
   // Avoid watermarked images from Unsplash Plus 
-  if (imgUrl.includes('plus.')) {
-    console.log("📜 LOG > fetchRandomImage > skipping");
-    // await wait(750); // prevent spam
-    return fetchRandomImage();
-  } else {
-    const imgRes = await fetch(imgUrl);
-    const blob = await imgRes.blob();
-    return blobToBase64(blob)
+  // if (!downloadUrl.includes('plus.')) {
+  //   console.log("📜 LOG > fetchRandomImage > skipping");
+  //   // await wait(750); // prevent spam
+  //   return fetchRandomImage();
+  // }
+  
+  const imgRes = await fetch(downloadUrl);
+  const blob = await imgRes.blob();
+  const asBase64 = await blobToBase64(blob)
+  //
+  const siteUrl = body.links.html;
+  const { name: authorName } = body.user;
+  const { width, height, color, created_at: createdAt, description, alt_description: altDescription, views, location, tags_preview } = body
+  //
+  return {
+    authorName,
+    asBase64,
+    width, height,
+    createdAt,
+    description,
+    altDescription,
+    views,
+    siteUrl,
+    color,
+    location, tags_preview
   }
 }
